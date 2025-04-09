d57829459cbdd8802d5a6ba59a1aa83472385f73
thelevihahn <76507529+thelevihahn@users.noreply.github.com>
2025-04-09 15:57:58 -0500

    Started cam events

--- CoOp.ts
+++ CoOp.ts
@@ -29,4 +29,46 @@ namespace scene {
         //UpdateCamera
         scene.centerCameraAt(x / playerCount, y / playerCount)
     }
+    
+    const exitCamHandlers: {kind: number, handler: (sprite: Sprite) => void}[]=[]
+    const watchedSprites: {kind: number, sprite: Sprite}[]=[]
+
+    // Register a handler that will be called when a sprite exits the screen
+    export function onExitCamera(kind: number, handler: (sprite: Sprite) => void){
+        exitCamHandlers.push({kind,handler})
+    }
+
+    // Watch an array of sprites of a specific kind for when they exit the camera view
+    export function watchSprites(kind: number, sprites: Sprite[]){
+        for(const sprite of sprites){
+            if (watchedSprites.find(s => s.sprite === sprite) === undefined){
+                watchedSprites.push({ kind, sprite })
+            }
+        }
+    }
+
+    // Check if a sprite is offscreen (ouside the camera view)
+    function isOffscreen(sprite: Sprite): boolean {
+        return (
+            sprite.right < scene.cameraProperty(CameraProperty.Left) ||
+            sprite.left > scene.cameraProperty(CameraProperty.Right) ||
+            sprite.bottom < scene.cameraProperty(CameraProperty.Top) ||
+            sprite.top > scene.cameraProperty(CameraProperty.Bottom)
+        )
+    }
+
+    control.runInParallel(() => {
+        while(true) {
+            for (const watched of watchedSprites) {
+                if (isOffscreen(watched.sprite)) {
+                    // Only tirgger handlers for sprites that match the specified allOfKind
+                    for (const camHandler of exitCamHandlers){
+                        if (camHandler.kind === watched.kind){
+                            camHandler.handler(watched.sprite)
+                        }
+                    }
+                }
+            }
+        }
+    })
 }
